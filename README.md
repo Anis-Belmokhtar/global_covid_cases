67ff31c65929497ddcc1a548e44d3cf56aab8615
Anis Belmokhtar <anisbelmokhtar@gmail.com>
2020-04-21 02:08:45 +0100

    added dockerfile, updated readme

--- README.md
+++ README.md
@@ -1,5 +1,22 @@
 # global_covid_cases
 
-Summary: Random crap
+Summary: This is a prototype app which shows the number of confirmed coronavirus cases in a country. 
+It uses a distributed database to store the result of api calls on the covid19API (https://documenter.getpostman.com/view/10808728/SzS8rjbc?version=latest); if the country you're looking for has an up-to-date record in the database, it will query the database. If not, it will request the covid19 api and update the database. 
+
+Instructions for containerising the app: 
+
+Instructions for hosting app on Kubernetes:
+
+Instructions for adding nodes to Kubernetes:
+
+Instructions for setting up Cassandra DB:
+
+Instructions for adding nodes to Cassandra cluster: 
+
+
+
+
+
+
 
 
--- myapp.py
+++ myapp.py
@@ -10,17 +10,21 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return '<h1>This is the starting page. Enter a country name in the url to find number of cases for Covid19.</h1>'
+    return '<h1>This is the starting page. Enter a country name in the url to find number of cases for Covid19.</h1>', 200
 
 
 def update_record_in_db(country_name, cases):
+    # Updates the DB with a record for the given country using the given cases, setting the date to 'today'. If the
+    # record is present, it will overwrite. If not, it will create a new record.
     today = date.today()
     session.execute(
         """Update countries.cases set cases={}, publish_date='{}' where name='{}'""".format(cases, today, country_name))
 
 
 def retrieve_record_from_api(country_name):
-    # GET latest record from external api, update record in DB.
+    # Retrieves the record from the external api for the given country. Returns a Python dictionary containing the
+    # number of cases (from the api), today's date and the given country name. Returns none if the api can't process
+    # the given country name.
     covid_url = 'https://api.covid19api.com/total/country/{country}/status/confirmed'.format(country=country_name)
     resp = requests.get(covid_url)
     if resp.ok:
@@ -33,10 +37,13 @@ def retrieve_record_from_api(country_name):
 
 
 def delete_record_from_db(country_name):
+    # Simply sends a DB command to delete the matching record from the DB.
     session.execute("""Delete from countries.cases where name='{}'""".format(country_name))
 
 
 def retrieve_record_from_db(country_name):
+    # Retrieves the record from the DB and returns the row with all its attributes accessible. Returns none if the
+    # record doesn't exist in the DB.
     records = session.execute("""Select * from countries.cases where name = '{}'""".format(country_name))
     if records:
         return records[0]
@@ -46,28 +53,35 @@ def retrieve_record_from_db(country_name):
 
 @app.route('/<country_name>', methods=['GET'])
 def get_case_by_country(country_name):
+    # Calls method to retrieve a record from the DB. If the record exists and is up-to-date (by comparing with
+    # current_date), returns the number of cases to the user. If either conditions fail, calls method to make an
+    # external api address, update the DB and return the cases.
     current_date = date.today()
     retrieved_record = retrieve_record_from_db(country_name)
     if retrieved_record and retrieved_record.publish_date == current_date:
-        return 'Successfully retrieved DB record: {}, {}'.format(retrieved_record.cases, retrieved_record.publish_date)
+        return 'Successfully retrieved DB record: {} cases, as of {}'.format(retrieved_record.cases,
+                                                                             retrieved_record.publish_date), 200
     else:
         api_record = retrieve_record_from_api(country_name)
         if api_record:
             update_record_in_db(country_name, api_record.get("cases"))
-            return 'Successfully retrieved api record and saved to DB: {}, {}'.format(api_record.get("cases"),
-                                                                                      api_record.get("publish_date"))
+            return 'Successfully retrieved api record and saved to DB: {} cases, as of {}'.format(
+                api_record.get("cases"),
+                api_record.get("publish_date")), 200
         else:
-            return 'Unable to find record for {}'.format(country_name)
+            return 'Unable to find record for {}'.format(country_name), 400
 
 
 @app.route('/<country_name>', methods=['DELETE'])
 def delete_country(country_name):
+    # Calls method to retrieve record from DB. If found, calls the delete method. If not, returns 'Record not found.'
     retrieved_record = retrieve_record_from_db(country_name)
     if retrieved_record:
         delete_record_from_db(country_name)
-        return 'Successfully deleted db record.'
+        return 'Successfully deleted db record.', 200
     else:
-        return 'Record not found.'
+        return 'Record not found.', 404
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
